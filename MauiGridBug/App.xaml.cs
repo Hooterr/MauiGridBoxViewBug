ad8741cb6cde5bf94f41d25233031b805df4c380
Maksymilian Lach <Maksymilian_Lach@next.co.uk>
2023-05-29 15:01:49 +0200

    Initail commit

---
+++ MauiGridBug/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiGridBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiGridBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiGridBug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
